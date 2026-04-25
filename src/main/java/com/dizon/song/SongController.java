9d51d0864541408226f4c5eed9750c07e8ef2b34
AndreDizon <cardizon.student@ua.edu.ph>
2026-04-25 18:17:11 +0800

    Add CORS cross origin support

--- src/main/java/com/dizon/song/SongController.java
+++ src/main/java/com/dizon/song/SongController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@CrossOrigin(origins = "*") // Allows your Render frontend to talk to this API@RestController
 @RequestMapping("/dizon/songs")
 public class SongController {
 
